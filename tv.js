1ee5fefc8d452da4e6ae2a7a4ec148c442148336
vivaana111 <86963066+vivaana111@users.noreply.github.com>
2022-10-01 11:21:06 +0200

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,39 @@
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+img = "";
+status ="";
+function modelLoaded(){
+    console.log("Model loaded !!! :)")
+    status = true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error,result){
+if (error) {
+     console.log(error);
+ }
+ console.log(results);
+}
+
+
+function preload(){
+img = loadImage('tv.jpeg');
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Tv room", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+
+    fill('FF0000');
+    text("Tv",320,120);
+    noFill();
+    stroke("FF0000")
+    rect(300,90,270,320);
+}
---
+++ vase.js
@@ -0,0 +1,38 @@
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+img = "";
+status ="";
+function modelLoaded(){
+    console.log("Model loaded !!! :)")
+    status = true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error,result){
+if (error) {
+     console.log(error);
+ }
+ console.log(results);
+}
+
+function preload(){
+img = loadImage('vase.jpeg');
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("vase", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+
+    fill('FF0000');
+    text("Mug",320,120);
+    noFill();
+    stroke("FF0000")
+    rect(300,90,270,320);
+}
